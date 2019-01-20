bd4752f75f425fb3a07abf77838aae9da384221a
eivindsimensen <eivind.simensen@knett.no>
2019-01-20 11:51:49 +0100

    Add files via upload

---
+++ e107_languages/Norwegian/admin/lan_notify.php
@@ -0,0 +1,44 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Norwegian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/01 20:17:19
+|
+|        $Author: Norwayman $
++---------------------------------------------------------------+
+*/
+
+define("NA_LAN_1", "Administrator oppdaterer passordet sitt");
+define("NA_LAN_2", "Administrator oppretter en ny bruker");
+define("NA_LAN_3", "Administrator aktiverer en ny bruker");
+define("NT_LAN_1", "Varsle");
+define("NT_LAN_2", "Motta varsel på");
+define("NU_LAN_1", "Brukerhendelser");
+define("NU_LAN_2", "Brukeren registrering");
+define("NU_LAN_3", "Brukeren Kontobekreftelse");
+define("NU_LAN_4", "Brukerpålogging");
+define("NU_LAN_5", "Bruker utlogging");
+define("NU_LAN_6", "Bruker sosial innlogging");
+define("NU_LAN_7", "Bruker sosial påmelding");
+define("NU_LAN_8", "Bruker ser på profil");
+define("NU_LAN_9", "Bruker redigerer profil");
+define("NS_LAN_1", "Sikkerhetshendelser");
+define("NS_LAN_2", "IP utestengt for flom området");
+define("NS_LAN_3", "IP utestengt for flere mislykkede påloggingsforsøk");
+define("NN_LAN_1", "Nyheter hendelser");
+define("NN_LAN_2", "Nyheter element levert av bruker");
+define("NN_LAN_3", "Nyheter artikkel skrevet av admin");
+define("NN_LAN_4", "Nyheter element redigert av admin");
+define("NN_LAN_5", "Nyheter element slettet av admin");
+define("NN_LAN_6", "Nyhetsvarsling utløst");
+define("NM_LAN_1", "E hendelser");
+define("NM_LAN_2", "Bulk e-post kjører fullstendig");
+define("NM_LAN_3", "E-postadresse =>");
+define("NF_LAN_1", "Filen hendelser");
+define("NF_LAN_2", "Fil lastet opp av brukeren");
+define("LAN_NOTIFY_01", "Arrangementer");
+
+
+?>
